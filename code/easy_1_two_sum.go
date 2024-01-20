900b33f47dcb0d5c2acec5c7a310552d207390b4
y-kakinuma <y-kakinuma@paronym.jp>
2024-01-20 23:07:55 +0900

    init

---
+++ code/easy_1_two_sum.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+package code
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	len := len(nums)
+	for i := 0; i < len; i++ {
+		for j := i + 1; j < len; j++ {
+			// if i == j {
+			// 	continue
+			// }
+			if (nums[i] + nums[j]) == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// @lc code=end
